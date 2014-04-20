c6fd8301e79c8319a01d6152e4de22629cea356b
Blaxcraft <golfboy2300@yahoo.com>
2014-04-20 15:02:44 -0700

    I GOOD AT WORLDGEN

--- BattleBane/src/net/mcshockwave/bbane/BattleBane.java
+++ BattleBane/src/net/mcshockwave/bbane/BattleBane.java
@@ -9,33 +9,30 @@ import org.bukkit.WorldType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BattleBane extends JavaPlugin {
-	
-	public static BattleBane ins;
+
+	public static BattleBane	ins;
 
 	public void onEnable() {
 		ins = this;
 		Bukkit.getPluginManager().registerEvents(new DefaultListener(), this);
-		
+
 		getCommand("bane").setExecutor(new Bane());
-		
+
 		Bukkit.createWorld(new WorldCreator("BattleBaneLobby"));
 		Bukkit.createWorld(new WorldCreator("BattleBaneArena"));
-		
-		WorldCreator world = new WorldCreator("BattleBaneWorld");
-		world.type(WorldType.NORMAL);
-		Bukkit.createWorld(world);
+		Bukkit.createWorld(new WorldCreator("BattleBaneArena").type(WorldType.NORMAL));
 	}
-	
+
 	public static World lob() {
 		return Bukkit.getWorld("BattleBaneLobby");
 	}
-	
+
 	public static World are() {
 		return Bukkit.getWorld("BattleBaneArena");
 	}
-	
+
 	public static World wor() {
 		return Bukkit.getWorld("BattleBaneWorld");
 	}
-	
+
 }
