8034208f3ce659de768be40eab8125d7bd3f1e64
Blaxcraft <golfboy2300@yahoo.com>
2014-04-20 20:27:54 -0700

    added new file

---
+++ BattleBane/src/net/mcshockwave/bbane/BBKit.java
@@ -0,0 +1,73 @@
+package net.mcshockwave.bbane;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashMap;
+
+public enum BBKit {
+
+	Civilian(
+		Material.WORKBENCH,
+		1,
+		0);
+
+	protected static HashMap<String, BBKit>	used	= new HashMap<>();
+
+	public Material								ico;
+	public int									am, da;
+	public String								name;
+
+	public ItemStack[]							kit;
+
+	private BBKit(Material ico, int am, int da, ItemStack... items) {
+		this.ico = ico;
+		this.am = am;
+		this.da = da;
+
+		this.kit = items;
+
+		this.name = name().replace('_', ' ');
+	}
+
+	public static BBKit getClassFor(Player p) {
+		return getClassFor(p.getName());
+	}
+
+	public static BBKit getClassFor(String s) {
+		for (BBKit cs : values()) {
+			if (cs.isKit(s)) {
+				return cs;
+			}
+		}
+		return null;
+	}
+
+	public void onUse(Player p) {
+		used.remove(p.getName());
+		used.put(p.getName(), this);
+	}
+
+	public void giveKit(Player p) {
+		p.getInventory().addItem(kit);
+
+		if (this == Civilian) {
+			p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+		}
+	}
+
+	public boolean isKit(Player p) {
+		return isKit(p.getName());
+	}
+
+	public boolean isKit(String s) {
+		if (!used.containsKey(s)) {
+			return false;
+		}
+		return used.get(s) == this;
+	}
+
+}
